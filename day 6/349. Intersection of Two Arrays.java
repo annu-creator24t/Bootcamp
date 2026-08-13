9d9fc1815c9fa0aee4ec03c48ecd9953ec765f49
Annu Tiwari <annu04t@gmail.com>
2026-08-13 09:40:11 +0530

    Day 6 homework

---
+++ day 6/349. Intersection of Two Arrays.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : nums2) {
+            if (map.containsKey(num)) {
+                list.add(num);
+                map.remove(num);
+            }
+        }
+
+        int[] result = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
+    }
+}
