a717f7c7943382e6c988425b746c5defe6a1548d
Annu Tiwari <annu04t@gmail.com>
2026-08-23 10:31:54 +0530

    Min stack

---
+++ day 14/155. Min Stack.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class MinStack {
+
+    private Stack<Long> stack;
+    private long min;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+
+    public void push(int val) {
+        if (stack.isEmpty()) {
+            stack.push((long) val);
+            min = val;
+        } else {
+            if (val < min) {
+                stack.push(2L * val - min);
+                min = val;
+            } else {
+                stack.push((long) val);
+            }
+        }
+    }
+
+    public void pop() {
+        long top = stack.pop();
+        if (top < min) {
+            min = 2 * min - top;
+        }
+    }
+
+    public int top() {
+        long top = stack.peek();
+        if (top < min) {
+            return (int) min;
+        }
+        return (int) top;
+    }
+
+    public int getMin() {
+        return (int) min;
+    }
+}
