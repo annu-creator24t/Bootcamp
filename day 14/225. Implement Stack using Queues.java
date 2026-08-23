2efa4fd8f360ffd28f6e3a284af9a4033e31df79
Annu Tiwari <annu04t@gmail.com>
2026-08-23 10:25:00 +0530

    Implement stack using queue

---
+++ day 14/225. Implement Stack using Queues.java
@@ -0,0 +1,46 @@
+ import java.util.*;
+
+class MyStack {
+
+    private Queue<Integer> q1;
+    private Queue<Integer> q2;
+
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q1.add(x);
+    }
+    public int pop() {
+        while (q1.size() > 1) {
+            q2.add(q1.remove());
+        }
+
+        int topElement = q1.remove(); 
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+
+        return topElement;
+    }
+    
+    public int top() {
+        while (q1.size() > 1) {
+            q2.add(q1.remove());
+        }
+
+        int topElement = q1.peek();
+        q2.add(q1.remove());
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+
+        return topElement;
+    }
+    
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
